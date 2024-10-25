285002469e1fb0c77c12afe54ebd47add0a3a037
Christian <christiancamiloriveralamadrid@gmail.com>
2024-10-25 17:31:26 -0500

    chore: adding scripts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./app/App";
@@ -10,9 +10,9 @@ import { Store } from "./app/store/Store";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={Store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
