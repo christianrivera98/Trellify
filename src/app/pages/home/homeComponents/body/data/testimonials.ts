898603ec3e4e258e58960bf5f0bdd1257a962511
Christian <christiancamiloriveralamadrid@gmail.com>
2024-10-30 22:44:25 -0500

    refactor: testimonials.ts

---
+++ src/app/pages/home/homeComponents/body/data/testimonials.ts
@@ -0,0 +1,20 @@
+export const testimonials = [
+    {
+      photo: 'https://res.cloudinary.com/ma-cloud/image/upload/v1729896138/findy/men_auyaef.jpg',
+      alt: 'Usuario 1',
+      quote: 'Trellify me ayudó a organizar mis proyectos de una forma que nunca imaginé. ¡Altamente recomendado!',
+      name: 'Juan Pérez',
+    },
+    {
+      photo: 'https://res.cloudinary.com/ma-cloud/image/upload/v1729896139/findy/woman_ng7gvr.jpg',
+      alt: 'Usuario 2',
+      quote: 'La interfaz es increíble y muy fácil de usar. Ahora puedo gestionar mi equipo sin complicaciones.',
+      name: 'Ana Gómez',
+    },
+    {
+      photo: 'https://res.cloudinary.com/ma-cloud/image/upload/v1729896138/findy/men_auyaef.jpg',
+      alt: 'Usuario 3',
+      quote: 'Me encanta cómo puedo personalizar mis tableros. Trellify ha mejorado mi productividad.',
+      name: 'Carlos Martínez',
+    },
+  ];
